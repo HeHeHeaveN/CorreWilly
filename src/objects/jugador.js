1cde2904a35fc21cf268a4f21d9f80e114900817
dario-bit <dario.uno@hotmail.com>
2019-10-31 14:31:17 +0100

    Probando sprite sheet

--- src/objects/jugador.js
+++ src/objects/jugador.js
@@ -1,10 +1,9 @@
 class Jugador extends  Phaser.GameObjects.Sprite {
     constructor(scene){
         super()        
-
-
-        
     }
 
-    
+    create() {
+        matterSprite: this.matter.add.sprite(100, 100, 'jugador', 4)
+    }
 }
--- src/states/juego.js
+++ src/states/juego.js
@@ -9,9 +9,13 @@ class juego extends Phaser.Scene {
 
     create() {
         this.add.image(800, 450, 'fondo')
+
+        //var jugadoractual = new jugador(); hay que llamarlo desde aqui
+
+        matterSprite: this.matter.add.sprite(100, 100, 'jugador', 1)  // implementar en jugador.js
     }
 
     update() {
-
+        
     }
 }
