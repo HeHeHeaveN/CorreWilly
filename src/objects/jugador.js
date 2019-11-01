c9d19ce519d45c39bc913ab1855b6ba91856ca39
dario-bit <dario.uno@hotmail.com>
2019-11-01 14:32:20 +0100

    Clase jugador

--- src/objects/jugador.js
+++ src/objects/jugador.js
@@ -1,9 +1,10 @@
-class Jugador extends  Phaser.GameObjects.Sprite {
-    constructor(scene){
-        super()        
+class jugador extends Phaser.GameObjects.Sprite {
+    constructor(scene) {
+        super(scene);
+        
     }
 
     create() {
-        matterSprite: this.matter.add.sprite(100, 100, 'jugador', 4)
+        matterSprite: this.matter.add.sprite(100, 100, 'spriteSheerJugador', 1);
     }
 }
--- src/states/juego.js
+++ src/states/juego.js
@@ -8,14 +8,14 @@ class juego extends Phaser.Scene {
     }
 
     create() {
-        this.add.image(800, 450, 'fondo')
+        this.add.image(800, 450, 'fondo');
 
-        //var jugadoractual = new jugador(); hay que llamarlo desde aqui
+        //let jugador = this.matter.add.sprite(100, 100, 'spriteSheerJugador', 1);
 
-        matterSprite: this.matter.add.sprite(100, 100, 'jugador', 1)  // implementar en jugador.js
+        let jugador = new jugador(this);
     }
 
     update() {
-        
+
     }
 }
