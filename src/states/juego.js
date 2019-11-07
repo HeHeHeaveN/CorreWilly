a4d1e949c537c8892e912de64a89117d7087e0e1
dario-bit <dario.uno@hotmail.com>
2019-11-07 13:57:02 +0100

    Controles

--- src/states/juego.js
+++ src/states/juego.js
@@ -9,24 +9,70 @@ class juego extends Phaser.Scene {
     }
 
     create() {
+
+        cursors = this.input.keyboard.createCursorKeys();
+
+        //animaciones
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('spriteSheerJugador', { start: 1, end: 10 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{ key: 'spriteSheerJugador', frame: 0 }],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('spriteSheerJugador', { start: 20, end: 30 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
         this.add.image(800, 450, 'fondo');
 
         //Jugador 1
         jugador = new Jugador(this, 100, 100);
-        jugador.sprite.setBounce(1);
+        jugador.sprite.setBounce(0.1);
 
 
         //Jugador 2
-        jugador2 = new Jugador(this, 300, 300);       
+        jugador2 = new Jugador(this, 300, 300);
 
     }
 
     update() {
 
+        //controles
 
+        if (cursors.left.isDown) {
+            jugador.sprite.setVelocityX(-400);
+
+            jugador.sprite.anims.play('left', true);
+        }
+        else if (cursors.right.isDown) {
+            jugador.sprite.setVelocityX(400);
+
+            jugador.sprite.anims.play('right', true);
+        }
+        else {
+            jugador.sprite.setVelocityX(0);
+
+            jugador.sprite.anims.play('turn');
+        }
+
+        if (cursors.up.isDown && jugador.sprite.body.touching.down)
+        {
+            jugador.sprite.setVelocityY(-500);
+        }
 
     }
 }
 
 var jugador;
 var jugador2;
+var cursors;
